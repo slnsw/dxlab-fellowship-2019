1f453bfd63471cc530e2652be5ca1aac84f81b74
Mauricio Giraldo <hello@mauriciogiraldo.com>
2020-03-29 09:06:51 -0400

    :space_invader: added gh-pages deployment

---
+++ deploy.sh
@@ -0,0 +1,24 @@
+    #!/usr/bin/env sh
+
+    # abort on errors
+    set -e
+
+    # build
+    npm run build
+
+    # navigate into the build output directory
+    cd dist
+
+    # deploy to custom domain
+    # echo 'www.example.com' > CNAME
+
+    git init
+    git add -A
+    git commit -m 'deploy'
+
+    # deploy to gh-pages
+    git push -f git@github.com:slnsw/dxlab-fellowship-2019.git master:gh-pages
+
+    cd -
+
+#
