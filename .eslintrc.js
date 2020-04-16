9f34d7fa6ef7c79fbdc9dd0d4b154b2f81ffbe6a
Mauricio Giraldo <hello@mauriciogiraldo.com>
2020-04-16 19:20:57 -0400

    :space_invader: added nuxt linter which is better for vue files / fixes #10

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,15 +1,22 @@
 module.exports = {
   root: true,
   env: {
+    browser: true,
     node: true
   },
   extends: [
+    '@nuxtjs',
+    'plugin:vue/recommended',
+    'prettier',
+    'prettier/vue',
+    'plugin:prettier/recommended',
     'plugin:vue/essential',
     'eslint:recommended',
     '@vue/typescript/recommended',
     '@vue/prettier',
     '@vue/prettier/@typescript-eslint'
   ],
+  plugins: ['prettier'],
   parserOptions: {
     ecmaVersion: 2020
   },
@@ -17,6 +24,7 @@ module.exports = {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     camelcase: 'off',
-    '@typescript-eslint/camelcase': ['error', { properties: 'never' }]
+    '@typescript-eslint/camelcase': ['error', { properties: 'never' }],
+    'unicorn/number-literal-case': 'off'
   }
 }
